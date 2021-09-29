ecdf719b278d3de77bd621ac1116f05803b26181
l-k-mist <dylan@vandenbosch.co.za>
2021-09-29 13:05:01 +0200

    start with dblClick trigger

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div @dblclick="end()" id="svg-wrapper">
+    <svg id="draw-svg"></svg>
+  </div>
+  <!-- <dev-panel></dev-panel> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+/* eslint-disable no-debugger */
+/* eslint-disable no-unused-vars */
+// TODO next: Must split flag into rect(path) and line
+import {
+  defineComponent,
+  onMounted,
+  watchEffect,
+  ref,
+  nextTick,
+  watch,
+  computed,
+} from "vue";
 
-export default {
+import { SVG, extend } from "@svgdotjs/svg.js";
+import "@/svg.draw.esm";
+
+import { screenToSVGPoint } from "@/helpers";
+import {
+  useMousePositionScreen,
+  useMousePositionSVG,
+  useWindowSize,
+} from "@/composables/WebApi";
+
+import DevPanel from "@/components/DevPanel";
+
+export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    // DevPanel
   },
-};
+  setup() {
+    const { windowWidth, windowHeight } = useWindowSize();
+
+    function viewBoxString(frame) {
+      return `${frame.x} ${frame.y} ${frame.width} ${frame.height}`;
+    }
+    let svg;
+    const { svgX, svgY } = useMousePositionSVG("draw-svg");
+    const { x, y } = useMousePositionScreen();
+    const positions = computed(() => {
+      return {
+        svgX: svgX.value,
+        svgY: svgY.value,
+        screenX: x.value,
+        screenY: y.value,
+      };
+    });
+    // watch(positions, (currentValue) => {
+    //   console.log(currentValue);
+    // });
+    const shipPosition = ref({ x: 40, y: 50 });
+
+    function end() {
+      console.log("dvdb - positions - positions", positions);
+    }
+    onMounted(() => {
+      var drawing = SVG("#draw-svg").size(`100%`, `100%`);
+      console.log("dvdb - onMounted - drawing", drawing);
+      var line = drawing
+        .polyline({
+          "stroke-width": 10,
+          stroke: "blue",
+          fill: "none",
+          drawCircles: true,
+        })
+        .draw();
+      line.on("drawpoint", () => {
+        console.log(positions.value);
+      });
+      drawing.on("dblClick", () => {
+        console.log("dvdb - drawing.on - dblClick");
+      });
+      console.log("dvdb - line.on - line", line);
+    });
+    return {
+      shipPosition,
+      end,
+    };
+  },
+});
 </script>
 
 <style>
+body {
+  overflow: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +99,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#svg-wrapper {
+  border: 4px dotted blue;
+  height: 90vh;
 }
 </style>
