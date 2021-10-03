71bc42a37f0eeb847e419b3b29f08da47ea6b5a1
l-k-mist <dylan@vandenbosch.co.za>
2021-10-03 09:41:12 +0200

    BaseButton

---
+++ src/baseComponents/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    type="button"
+    class="
+      inline-flex
+      items-center
+      px-6
+      py-3
+      border border-transparent
+      text-base
+      font-medium
+      rounded-md
+      shadow-sm
+      text-white
+      bg-indigo-600
+      hover:bg-indigo-700
+      focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+      absolute
+      top-4
+      right-4
+      z-20
+    "
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
