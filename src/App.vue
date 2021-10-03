6dd5b5f50efbb0950fd17b1316c30e49aa9d36a5
l-k-mist <dylan@vandenbosch.co.za>
2021-10-03 09:24:40 +0200

    test with button

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,33 @@
   <div id="svg-chart" ref="svgChart">
     <chart ref="chartComponent" />
     <svg-draw-route v-if="true" @new-coords="handleNewCoords" />
+    <button
+      type="button"
+      class="
+        inline-flex
+        items-center
+        px-6
+        py-3
+        border border-transparent
+        text-base
+        font-medium
+        rounded-md
+        shadow-sm
+        text-white
+        bg-indigo-600
+        hover:bg-indigo-700
+        focus:outline-none
+        focus:ring-2
+        focus:ring-offset-2
+        focus:ring-indigo-500
+        absolute
+        top-4
+        right-4
+        z-20
+      "
+    >
+      Draw Route
+    </button>
   </div>
 </template>
 
